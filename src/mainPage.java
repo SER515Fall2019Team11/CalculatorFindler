4ebe4c51c194b748dba1e774bb416973c802bf8d
Yansong Peng <ypeng30@asu.edu>
2019-10-03 12:34:16 -0700

    First UI auto layout done:)

--- src/mainPage.java
+++ src/mainPage.java
@@ -10,6 +10,8 @@ import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class mainPage extends JFrame {
 
@@ -58,26 +60,35 @@ public class mainPage extends JFrame {
 			}
 		});
 		btnTeacher.setFont(new Font("Stencil", Font.PLAIN, 17));
+		
+		JLabel lblWelcomePleaseSelect = new JLabel("Welcome! Please select your role :)");
+		lblWelcomePleaseSelect.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWelcomePleaseSelect.setFont(new Font("Showcard Gothic", Font.PLAIN, 17));
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-					.addContainerGap(170, Short.MAX_VALUE)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(88)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnStudent, GroupLayout.PREFERRED_SIZE, 281, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnTeacher, GroupLayout.PREFERRED_SIZE, 281, GroupLayout.PREFERRED_SIZE))
-					.addGap(161))
+						.addComponent(btnTeacher, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 436, Short.MAX_VALUE)
+						.addComponent(btnStudent, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 436, Short.MAX_VALUE))
+					.addGap(88))
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addGap(140)
+					.addComponent(lblWelcomePleaseSelect, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(140))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(36)
-					.addComponent(btnStudent, GroupLayout.PREFERRED_SIZE, 71, GroupLayout.PREFERRED_SIZE)
-					.addGap(57)
-					.addComponent(btnTeacher, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(69, Short.MAX_VALUE))
+					.addContainerGap()
+					.addComponent(lblWelcomePleaseSelect, GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE)
+					.addGap(38)
+					.addComponent(btnStudent, GroupLayout.DEFAULT_SIZE, 70, Short.MAX_VALUE)
+					.addGap(32)
+					.addComponent(btnTeacher, GroupLayout.DEFAULT_SIZE, 69, Short.MAX_VALUE)
+					.addGap(46))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
-
 }
