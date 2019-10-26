2434dba377e49c498da9d9cc6edd90f67527e0d8
ngjain <54864607+ngjain@users.noreply.github.com>
2019-10-26 14:14:56 -0700

    Login Page for teacher and student

--- Main.java
+++ Main.java
@@ -13,71 +13,128 @@ import java.awt.event.ActionEvent;
 
 public class Main extends JFrame {
 
-	private JPanel contentPane;
+	boolean m_bExit = false;
+	JLabel jLabel1 = new JLabel();
+	JLabel jLabel2 = new JLabel();
+	JButton loginButton = new JButton();
+	JButton buttonExit = new JButton();
+	JTextField UserNameText = new JTextField();
+	JPasswordField PasswordText = new JPasswordField();
+	JRadioButton StudentRadio = new JRadioButton();
+	JRadioButton InstructorRadio = new JRadioButton();
+	ButtonGroup buttonGroup1 = new ButtonGroup();
+	
+	private String UserBox = null;
+	private USER_TYPE UserType = USER_TYPE.Student;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Main frame = new Main();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+	public Login() {
+		try {
+			jbInit();
+			setSize(300, 300);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public Main() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 640, 360);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		
-		JButton btnStudent = new JButton("Student");
-		btnStudent.addActionListener(new ActionListener() {
+	private void jbInit() throws Exception {
+		this.getContentPane().setLayout(null);
+		jLabel1.setText("UserName");
+		jLabel1.setBounds(new Rectangle(26, 52, 80, 18));
+		jLabel2.setText("Password");
+		jLabel2.setBounds(new Rectangle(23, 119, 80, 18));
+		loginButton.setText("Login");
+		loginButton.setBounds(new Rectangle(31, 212, 85, 28));
+		loginButton.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				studentInput student = new studentInput();
-				student.setVisible(true);
+				loginButton_actionPerformed(e);
 			}
 		});
-		btnStudent.setFont(new Font("Stencil", Font.PLAIN, 17));
-		
-		JButton btnTeacher = new JButton("Teacher");
-		btnTeacher.addActionListener(new ActionListener() {
+		buttonExit.setText("Exit");
+		buttonExit.setBounds(new Rectangle(180, 211, 97, 28));
+		buttonExit.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				teacherInput teacher = new teacherInput();
-				teacher.setVisible(true);
+				buttonExit_actionPerformed(e);
 			}
 		});
-		btnTeacher.setFont(new Font("Stencil", Font.PLAIN, 17));
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-					.addContainerGap(170, Short.MAX_VALUE)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnStudent, GroupLayout.PREFERRED_SIZE, 281, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnTeacher, GroupLayout.PREFERRED_SIZE, 281, GroupLayout.PREFERRED_SIZE))
-					.addGap(161))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(36)
-					.addComponent(btnStudent, GroupLayout.PREFERRED_SIZE, 71, GroupLayout.PREFERRED_SIZE)
-					.addGap(57)
-					.addComponent(btnTeacher, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(69, Short.MAX_VALUE))
-		);
-		contentPane.setLayout(gl_contentPane);
+		UserNameText.setBounds(new Rectangle(119, 52, 144, 22));
+		PasswordText.setBounds(new Rectangle(118, 119, 147, 22));
+		StudentRadio.setSelected(true);
+		StudentRadio.setText("Student");
+		StudentRadio.setBounds(new Rectangle(37, 164, 103, 26));
+		InstructorRadio.setText("Instructor");
+		InstructorRadio.setBounds(new Rectangle(177, 162, 103, 26));
+		this.getContentPane().add(jLabel1, null);
+		this.getContentPane().add(jLabel2, null);
+		this.getContentPane().add(loginButton, null);
+		this.getContentPane().add(buttonExit, null);
+		this.getContentPane().add(UserNameText, null);
+		this.getContentPane().add(PasswordText, null);
+		this.getContentPane().add(StudentRadio, null);
+		this.getContentPane().add(InstructorRadio, null);
+		buttonGroup1.add(StudentRadio);
+		buttonGroup1.add(InstructorRadio);
 	}
 
+	void loginButton_actionPerformed(ActionEvent e) {
+		BufferedReader file;
+		m_bExit = false;
+		System.out.println("login clicked");
+		try {
+			if (StudentRadio.isSelected() == true)
+			{
+				UserType = USER_TYPE.Student;
+				file = new BufferedReader(new FileReader("StuInfo.txt"));
+			} else// instructor
+			{
+				UserType = USER_TYPE.Instructor;
+				file = new BufferedReader(new FileReader("InsInfor.txt"));
+			}
+			UserBox = UserNameText.getText();
+			String PasswordBox = new String(PasswordText.getPassword());
+			String LoginName = null;
+			String aline = null, UserName = null, Password = null;
+			while ((aline = file.readLine()) != null) {
+				UserName = GetUserName(aline);
+				Password = GetPassword(aline);
+				if (UserName.compareTo(UserBox) == 0 && Password.compareTo(PasswordBox) == 0)
+					LoginName = UserName;
+			}
+			if (LoginName != null) {
+				this.hide();
+			}
+		} catch (Exception ee) {
+			;
+		}
+
+	}
+
+	
+	private String GetUserName(String aline) {
+		int Sep = aline.lastIndexOf(':');
+		return aline.substring(0, Sep);
+	}
+
+
+	private String GetPassword(String aline) {
+		int Sep = aline.lastIndexOf(':');
+		return aline.substring(Sep + 1, aline.length());
+	}
+
+	public String GetUserName() {
+		return UserBox;
+	}
+
+	
+	public USER_TYPE GetUserType() {
+		return UserType;
+	}
+
+	public boolean isExit() {
+		return m_bExit;
+	}
+
+	void buttonExit_actionPerformed(ActionEvent e) {
+		m_bExit = true;
+		hide();
+	}
 }
