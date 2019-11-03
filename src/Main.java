631470c66a68f38bc245399524253d8948828c51
ngjain <54864607+ngjain@users.noreply.github.com>
2019-11-03 14:53:10 -0700

    Changing Main to include restructuring
    Input from the owner the login and teacher page are being restructures to include design patterns

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		while (true) {
+			boolean bExit = false;
+			bExit = Facade.login(userinfoitem);
+			if (bExit)
+				break;
+			theFacade.createUser(userinfoitem);
+			theFacade.attachCourseToUser();
+			if (userinfoitem.UserType == UserInfoItem.USER_TYPE.Student) // if is a student remind him of the due date
+				theFacade.remind();
+			boolean bLogout = false;
+			while (!bLogout) {
+				bLogout = theFacade.selectCourse();
+				if (bLogout)
+					break;
+				bLogout = theFacade.courseOperation();
+			}
+		}
+	}
+}
