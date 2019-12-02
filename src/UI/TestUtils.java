b4444996f5371f0a5909d3977b8ff5fb2a3fabe0
ngjain <54864607+ngjain@users.noreply.github.com>
2019-12-02 14:22:48 -0700

    Removing unnecessary imports and adding author

--- src/UI/TestUtils.java
+++ src/UI/TestUtils.java
@@ -1,7 +1,11 @@
 /*
  * TestUtils
- */
- 
+ Author: Jain Nayan
+ Derived From: Ichiro Suzuki
+ This piece of code has been derived from 
+ https://www.javaworld.com/article/2073056/automate-gui-tests-for-swing-applications.html?page=2
+ It includes some test functionalities that are beyond the scope of this project.
+*/
 package UI;
 
 import java.awt.*;
@@ -12,6 +16,7 @@ public class TestUtils {
 	static int counter;
 
 	public static Component getChildNamed(Component parent, String name) {
+
 		if (name.equals(parent.getName())) { return parent; }
 
 		if (parent instanceof Container) {
@@ -28,49 +33,6 @@ public class TestUtils {
 		return null;
 	}
 
-	public static Component getChildIndexed(
-			Component parent, String klass, int index) {
-		counter = 0;
-
-		// Step in only owned windows and ignore its components in JFrame
-		if (parent instanceof Window) {
-			Component[] children = ((Window)parent).getOwnedWindows();
-
-			for (int i = 0; i < children.length; ++i) {
-				// take only active windows
-				if (children[i] instanceof Window &&
-						!((Window)children[i]).isActive()) { continue; }
-
-				Component child = getChildIndexedInternal(
-						children[i], klass, index);
-				if (child != null) { return child; }
-			}
-		}
-
-		return null;
 	}
 
-	private static Component getChildIndexedInternal(
-			Component parent, String klass, int index) {
-
-		if (parent.getClass().toString().endsWith(klass)) {
-			if (counter == index) { return parent; }
-			++counter;
-		}
-
-		if (parent instanceof Container) {
-			Component[] children = (parent instanceof JMenu) ?
-					((JMenu)parent).getMenuComponents() :
-					((Container)parent).getComponents();
-
-			for (int i = 0; i < children.length; ++i) {
-				Component child = getChildIndexedInternal(
-						children[i], klass, index);
-				if (child != null) { return child; }
-			}
-		}
-		
-		return null;
-	}
-}
 
