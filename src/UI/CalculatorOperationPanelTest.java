f8c1eb96bb4d8afd04691d2b9064a66e3689caa7
ngjain <54864607+ngjain@users.noreply.github.com>
2019-12-02 14:36:20 -0700

    Updating authors

--- src/UI/CalculatorOperationPanelTest.java
+++ src/UI/CalculatorOperationPanelTest.java
@@ -1,5 +1,9 @@
 package UI;
-
+/* 
+ Author: Jain Nayan
+ Built for: SER 515, Professor Findler
+Date 12/1/2109
+*/
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
